902f6a212188632d4c29a324a518eb93e90b1b8f
outsource.phanupong <outsurce.phanupong@csc.co.th>
2018-09-26 17:47:26 +0700

    Day 2 Finished

---
+++ database/factories/Post.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Post;
+use Faker\Generator as Faker;
+
+$factory->define(Post::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence(),
+        'content' => $faker->paragraph()
+    ];
+});
